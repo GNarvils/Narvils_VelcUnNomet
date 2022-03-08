59e6dd2f8e473823b75e610b20232410fe36fbc3
Narvils <gustavs.gn@gmail.com>
2022-03-08 14:17:55 +0200

    Skripti

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	private RectTransform velkObjRectTransf;
+	private CanvasGroup kanvasGrupa;
+	public Objekti objektuSkripts;
+	void Start () {
+		kanvasGrupa = GetComponent<CanvasGroup> ();
+		velkObjRectTransf = GetComponent<RectTransform> ();
+	}
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais kliksis uz objekta!");
+	}
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakts objekta vilksana!");
+		kanvasGrupa.alpha = 0.6F;
+		kanvasGrupa.blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData notikums){
+	   velkObjRectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Objekta vilksanas partraukta!");
+	}
+}
